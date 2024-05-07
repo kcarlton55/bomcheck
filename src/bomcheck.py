46eb59b3d1fc5762a059b8b83b006dff2abab513
Kenneth Carlton <kencarlton55@gmail.com>
2024-05-06 19:50:17 -0500

    update help files

--- src/bomcheck.py
+++ src/bomcheck.py
@@ -1570,7 +1570,7 @@ def view_help(help_type='bomcheck_help', version='master', dbdic=None):
         cfg.update(get_bomcheckcfg(dbdic['cfgpathname']))
 
     d = {'bomcheck_help': 'https://htmlpreview.github.io/?https://github.com/'
-             'kcarlton55/bomcheck/blob/' + version + '/help_files/bomcheck_help.html',
+             'kcarlton55/bomcheck/blob/' + version + '/help_files/bomcheck_help_section1.html',
          'bomcheckgui_help': 'https://htmlpreview.github.io/?https://github.com/'
              'kcarlton55/bomcheckgui/blob/' + version +'/help_files/bomcheckgui_help.html',
          'bomcheck_troubleshoot': 'https://htmlpreview.github.io/?https://github.com/'
